16acaa451bbb1bb2cb823b57b7da685ccc836703
jaypark <pjhyun1990@gmail.com>
2023-10-28 13:51:40 +0900

    init project

---
+++ src/main/java/kr/jay/pilotprojcet/PilotProjcetApplication.java
@@ -0,0 +1,13 @@
+package kr.jay.pilotprojcet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotProjcetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilotProjcetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/jay/pilotprojcet/PilotProjcetApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.jay.pilotprojcet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilotProjcetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
