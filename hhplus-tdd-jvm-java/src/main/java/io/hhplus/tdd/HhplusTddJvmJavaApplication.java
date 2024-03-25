eaa524798ed4f8602e19dd8db1b4993f7114c5e3
jaypark <pjhyun1990@gmail.com>
2024-03-25 23:45:15 +0900

    init hhplus tdd project

---
+++ hhplus-tdd-jvm-java/src/main/java/io/hhplus/tdd/HhplusTddJvmJavaApplication.java
@@ -0,0 +1,13 @@
+package io.hhplus.tdd.hhplustddjvmjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusTddJvmJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhplusTddJvmJavaApplication.class, args);
+	}
+
+}
---
+++ hhplus-tdd-jvm-java/src/test/java/io/hhplus/tdd/hhplustddjvmjava/HhplusTddJvmJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hhplus.tdd.hhplustddjvmjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusTddJvmJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
