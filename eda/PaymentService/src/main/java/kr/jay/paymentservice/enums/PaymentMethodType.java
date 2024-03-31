b8b2e27fd7c62a72133285b317553bf438211edd
jaypark <pjhyun1990@gmail.com>
2024-03-31 20:45:46 +0900

    payment 도메인 정의

---
+++ eda/PaymentService/src/main/java/kr/jay/paymentservice/enums/PaymentMethodType.java
@@ -0,0 +1,12 @@
+package kr.jay.paymentservice.enums;
+
+/**
+ * PaymentMethodType
+ *
+ * @author jaypark
+ * @version 1.0.0
+ * @since 3/31/24
+ */
+public enum PaymentMethodType {
+    CREDIT_CARD,
+}
