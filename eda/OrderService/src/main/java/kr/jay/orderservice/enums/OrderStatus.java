39b68b19d239d67feee4b98e9e0041355b474cd1
jaypark <pjhyun1990@gmail.com>
2024-04-05 23:13:59 +0900

    order 도메인 정의

---
+++ eda/OrderService/src/main/java/kr/jay/orderservice/enums/OrderStatus.java
@@ -0,0 +1,14 @@
+package kr.jay.orderservice.enums;
+
+/**
+ * OrderStatus
+ *
+ * @author jaypark
+ * @version 1.0.0
+ * @since 4/5/24
+ */
+public enum OrderStatus {
+    INITIATED,
+    PAYMENT_COMPLETED,
+    DELIVERY_REQUESTED,
+}
