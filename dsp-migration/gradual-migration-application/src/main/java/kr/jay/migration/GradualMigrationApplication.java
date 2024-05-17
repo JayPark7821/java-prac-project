97e011d90fb87da9815a6db67b966e6bc52f03ae
jaypark <pjhyun1990@gmail.com>
2024-05-18 01:19:43 +0900

    마이그레이션 서버 datasource 설정

---
+++ dsp-migration/gradual-migration-application/src/main/java/kr/jay/migration/GradualMigrationApplication.java
@@ -0,0 +1,20 @@
+package kr.jay.migration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * GradualMigrationApplication
+ *
+ * @author jaypark
+ * @version 1.0.0
+ * @since 5/17/24
+ */
+@SpringBootApplication
+public class GradualMigrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradualMigrationApplication.class, args);
+    }
+
+}
---
+++ dsp-migration/gradual-migration-application/src/main/java/kr/jay/migration/config/LegacyAdJpaConfig.java
@@ -0,0 +1,70 @@
+package kr.jay.migration.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.persistence.EntityManagerFactory;
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * LegacyAdJpaConfig
+ *
+ * @author jaypark
+ * @version 1.0.0
+ * @since 5/16/24
+ */
+@Configuration
+@EnableJpaRepositories(
+    basePackages = "kr.jay.migration.domain.legacy",
+    entityManagerFactoryRef = "legacyAdEntityManagerFactory",
+    transactionManagerRef = "legacyAdTransactionManager"
+)
+public class LegacyAdJpaConfig {
+
+    @Bean("legacyAdDataSource")
+    @ConfigurationProperties(prefix = "spring.jpa.legacy-ad.hikari")
+    public DataSource dataSource(){
+        return DataSourceBuilder.create()
+            .type(HikariDataSource.class).build();
+    }
+
+    @Bean("legacyAdJpaProperties")
+    @ConfigurationProperties(prefix = "spring.jpa.legacy-ad.properties")
+    public Properties legacyJpaProperties(){
+        return new Properties();
+    }
+
+    @Bean("legacyAdEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean legacyAdEntityManagerFactory(
+        EntityManagerFactoryBuilder builder,
+        @Qualifier("legacyAdDataSource") DataSource dataSource,
+        @Qualifier("legacyAdJpaProperties")Properties jpaProperties
+    ){
+        LocalContainerEntityManagerFactoryBean factoryBean = builder
+            .dataSource(dataSource)
+            .packages("kr.jay.migration.domain.legacy")
+            .build();
+
+        factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        factoryBean.setJpaProperties(jpaProperties);
+        return factoryBean;
+    }
+
+    @Bean("legacyAdTransactionManager")
+    public PlatformTransactionManager legacyTransactionManager(
+        @Qualifier("legacyAdEntityManagerFactory") EntityManagerFactory entityManagerFactory
+    ){
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
