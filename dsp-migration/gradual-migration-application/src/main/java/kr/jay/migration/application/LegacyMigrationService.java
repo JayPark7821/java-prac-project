77b35bcd9a9436545f330a13d286fc69a09d078f
jaypark <pjhyun1990@gmail.com>
2024-05-20 23:35:09 +0900

    마이그레이션 로직 구현

--- dsp-migration/gradual-migration-application/src/main/java/kr/jay/migration/application/LegacyMigrationService.java
+++ dsp-migration/gradual-migration-application/src/main/java/kr/jay/migration/application/LegacyMigrationService.java
@@ -1,9 +1,10 @@
 package kr.jay.migration.application;
 
+import jakarta.persistence.EntityNotFoundException;
 import kr.jay.migration.domain.legacy.DeletableEntity;
 import kr.jay.migration.domain.recent.MigratedEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Service;
 
 /**
  * LegacyMigrationService
@@ -12,7 +13,9 @@ import org.springframework.stereotype.Service;
  * @version 1.0.0
  * @since 5/18/24
  */
-public abstract class LegacyMigrationService<Legacy extends DeletableEntity, Recent extends MigratedEntity> implements MigrationService{
+@Slf4j
+public abstract class LegacyMigrationService<Legacy extends DeletableEntity, Recent extends MigratedEntity> implements
+    MigrationService {
 
     protected LegacyConverter<Legacy, Recent> converter;
     protected CrudRepository<Legacy, Long> legacyRepository;
@@ -30,6 +33,33 @@ public abstract class LegacyMigrationService<Legacy extends DeletableEntity, Rec
 
     @Override
     public boolean migrate(Long id) {
-        return false ;
+        try {
+            migrate(findLegacy(id));
+            return true;
+        } catch (RuntimeException e) {
+            log.error("migration error", e);
+            return false;
+        }
+    }
+
+    protected void migrate(Legacy legacy) {
+        if(legacy.isDeleted()){
+            recentRepository.findById(legacy.getId())
+                .ifPresent(recentRepository::delete);
+        }else{
+            saveRecent(getConvert(legacy));
+        }
+    }
+
+    private void saveRecent(Recent recent) {
+        recentRepository.save(recent);
+    }
+
+    private Recent getConvert(Legacy legacy) {
+        return converter.convert(legacy);
+    }
+
+    private Legacy findLegacy(Long id) {
+        return legacyRepository.findById(id).orElseThrow(EntityNotFoundException::new);
     }
 }
