214ab3f40c77b04eb0d5646a30320e07f0dfe8e3
jaypark <pjhyun1990@gmail.com>
2024-07-24 14:12:08 +0900

    toby spring base project

---
+++ toby-spring/src/main/java/kr/jay/tobyspring/PaymentService.java
@@ -0,0 +1,49 @@
+package kr.jay.tobyspring;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * PaymentService
+ *
+ * @author jaypark
+ * @version 1.0.0
+ * @since 7/24/24
+ */
+public class PaymentService {
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount)
+        throws IOException {
+        // 환율 조회
+        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String response = br.lines().collect(Collectors.joining());
+        br.close();
+        ObjectMapper mapper = new ObjectMapper();
+        ExRateData data = mapper.readValue(response, ExRateData.class);
+        BigDecimal exRate = data.rates().get("KRW");
+
+        // 금액 계산
+        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+        // 유효 시간 계산
+        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
+        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+    }
+
+    public static void main(String[] args) throws IOException {
+        PaymentService paymentService = new PaymentService();
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println(payment);
+    }
+
+}
