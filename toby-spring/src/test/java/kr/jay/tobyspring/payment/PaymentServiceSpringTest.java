5ea8898815c675210aac360f9f74787861a93e66
jaypark <pjhyun1990@gmail.com>
2024-07-31 23:18:52 +0900

    toby spring test with spring di

--- toby-spring/src/test/java/kr/jay/tobyspring/payment/PaymentServiceSpringTest.java
+++ toby-spring/src/test/java/kr/jay/tobyspring/payment/PaymentServiceSpringTest.java
@@ -6,10 +6,12 @@ import java.math.BigDecimal;
 import kr.jay.tobyspring.ObjectFactory;
 import kr.jay.tobyspring.TestObjectFactory;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * PaymentServiceTest
@@ -18,13 +20,15 @@ import org.springframework.test.context.ContextConfiguration;
  * @version 1.0.0
  * @since 7/30/24
  */
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestObjectFactory.class)
 class PaymentServiceSpringTest {
+
+    @Autowired
+    PaymentService paymentService;
 
     @Test
     void prepare() throws Exception {
-        BeanFactory beanFactory = new AnnotationConfigApplicationContext(TestObjectFactory.class);
-        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
-
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
 
         assertThat(payment.getExRate()).isEqualByComparingTo(BigDecimal.valueOf(1_000));
