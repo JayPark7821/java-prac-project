1d191b5d8c1bae3268dabce6cfb31bd6a0f35c46
jaypark <pjhyun1990@gmail.com>
2024-09-02 23:26:49 +0900

    init rabbitMq prac consumer prj

---
+++ rabbitmq-prac/consumer/src/test/kotlin/kr/jay/consumer/ConsumerApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.jay.consumer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
